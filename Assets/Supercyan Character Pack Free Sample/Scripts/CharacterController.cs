e53ca91aa8698a1ce7d8c5eba312d976f128c9f0
Trishla Shah <trishla.s2311@gmail.com>
2020-03-30 22:07:01 -0500

    character

---
+++ Assets/Supercyan Character Pack Free Sample/Scripts/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 25.0f;
+    public float rotationSpeed = 90;
+    public float force = 700f;
+
+    Rigidbody rb;
+    Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            rb.velocity += this.transform.forward * speed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            rb.velocity -= this.transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            t.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
+        else if (Input.GetKey(KeyCode.A))
+            t.rotation *= Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            rb.velocity += this.transform.up * speed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            rb.velocity -= this.transform.up * speed * Time.deltaTime;
+    }
+}
