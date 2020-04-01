ae4083a036b33a61cd5fe7026b2ffed70ca195e4
Kevin Andrews <kdrews34@gmail.com>
2020-04-01 01:59:03 -0500

    working demo level

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+        charController = GetComponent<CharacterController>();
+    }
+
+    public void resetTransform()
+    {
+        transform.position = originalPosition;
+        transform.rotation = originalRotation;
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void LateUpdate() {
+        if (transform.position.y < 2f)
+            resetTransform();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+    }
+
+    private void JumpInput()
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
---
+++ Assets/Scripts/colorSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorSwitch : MonoBehaviour
+{
+    private bool on;
+    public string baseColor;
+    Color gray;
+    Color red;
+    Color blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gray = Color.gray;
+        red = Color.red;
+        blue = Color.blue;
+        GetComponent<Collider>().isTrigger = true;
+        GetComponent<Renderer>().material.color = gray;
+        on = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    if (Input.GetKeyDown(KeyCode.E) && baseColor == "red") {
+        if (on){
+            GetComponent<Collider>().isTrigger = true;
+            GetComponent<Renderer>().material.color = gray;
+            on = false;
+        }
+        else{
+            GetComponent<Collider>().isTrigger = false;
+            GetComponent<Renderer>().material.color = red; 
+            on = true;
+        }
+    }
+    else if (Input.GetKeyDown(KeyCode.R) && baseColor == "blue") {
+        if (on){
+            GetComponent<Collider>().isTrigger = true;
+            GetComponent<Renderer>().material.color = gray;
+            on = false;
+        }
+        else{
+            GetComponent<Collider>().isTrigger = false;
+            GetComponent<Renderer>().material.color = blue; 
+            on = true;
+        }
+    }
+}
+}
