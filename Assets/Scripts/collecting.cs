7812a00c6de3035271a62be8f205144dddeb1a9e
Kevin Andrews <kdrews34@gmail.com>
2020-04-13 23:44:40 -0500

    added simple UI, new color collection

---
+++ Assets/Scripts/collecting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collecting : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject nextColor;
+    public GameObject colorSound;
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name == "Player"){
+            colorSound.GetComponent<AudioSource>().Play();
+            Destroy(this.gameObject);
+            var tempColor = nextColor.GetComponent<RawImage>().color;
+            tempColor.a = 0.4f;
+            nextColor.GetComponent<RawImage>().color = tempColor;
+            
+        }
+    }
+}
--- Assets/Scripts/colorSwitch.cs
+++ Assets/Scripts/colorSwitch.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class colorSwitch : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject RedUI;
+    public GameObject BlueUI;
 
     private bool redblue;
     public string baseColor;
@@ -33,46 +36,42 @@ public class colorSwitch : MonoBehaviour
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
-            if (redblue && baseColor == "red"){
+            if (redblue && baseColor == "red"){ //turn red off
                 GetComponent<AudioSource>().Play();
                 GetComponent<Collider>().isTrigger = true;
                 GetComponent<Renderer>().material.color = gray;
+                var tempColor = RedUI.GetComponent<RawImage>().color;
+                tempColor.a = 0.4f;
+                RedUI.GetComponent<RawImage>().color = tempColor;
                 redblue = false;
             }
-            else if (!redblue && baseColor == "red"){
+            else if (!redblue && baseColor == "red"){ //turn red on
                 GetComponent<AudioSource>().Play();
                 GetComponent<Collider>().isTrigger = false;
                 GetComponent<Renderer>().material.color = red;
+                var tempColor = RedUI.GetComponent<RawImage>().color;
+                tempColor.a = 1f;
+                RedUI.GetComponent<RawImage>().color = tempColor;
                 redblue = true;
             }
-            else if (!redblue && baseColor == "blue"){
+            else if (!redblue && baseColor == "blue"){ //turn blue off
                 GetComponent<AudioSource>().Play();
                 GetComponent<Collider>().isTrigger = true;
                 GetComponent<Renderer>().material.color = gray;
+                var tempColor = BlueUI.GetComponent<RawImage>().color;
+                tempColor.a = 0.4f;
+                BlueUI.GetComponent<RawImage>().color = tempColor;
                 redblue = true;
             }
-            else if (redblue && baseColor == "blue"){
+            else if (redblue && baseColor == "blue"){ //turn blue on;
                 GetComponent<AudioSource>().Play();
                 GetComponent<Collider>().isTrigger = false;
                 GetComponent<Renderer>().material.color = blue;
+                var tempColor = BlueUI.GetComponent<RawImage>().color;
+                tempColor.a = 1f;
+                BlueUI.GetComponent<RawImage>().color = tempColor;
                 redblue = false;
             }
         }
-        // else if (Input.GetKeyDown(KeyCode.R) && baseColor == "blue") {
-        // GetComponent<AudioSource>().Play();
-
-        //     if (redblue){
-        //         GetComponent<AudioSource>().Play();
-        //         GetComponent<Collider>().isTrigger = true;
-        //         GetComponent<Renderer>().material.color = gray;
-        //         redblue = false;
-        //     }
-        //     else{
-        //         GetComponent<AudioSource>().Play();
-        //         GetComponent<Collider>().isTrigger = false;
-        //         GetComponent<Renderer>().material.color = blue;
-        //         redblue = true;
-        //     }
-        // }
     }
 }
