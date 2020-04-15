719ce8fa366a03c6c7b244800550f686d1c7db72
Kevin Andrews <kdrews34@gmail.com>
2020-04-14 21:37:23 -0500

    added npc audio and collider/rigidbody

---
+++ Assets/npcScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void OnTriggerExit(Collider other) {
+        GetComponent<AudioSource>().Stop();
+    }
+}
