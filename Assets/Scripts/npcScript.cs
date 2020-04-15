f2013167d3053e950953459c3893d1da232e2665
fmarti35 <fmarti35@uic.edu>
2020-04-14 22:12:30 -0500

    Dialogue

--- Assets/Scripts/npcScript.cs
+++ Assets/Scripts/npcScript.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class npcScript : MonoBehaviour
 {
+    [SerializeField] public GameObject uiObject;
+
     private void OnTriggerEnter(Collider other) {
+        uiObject.SetActive(true);
         GetComponent<AudioSource>().Play();
     }
 
     private void OnTriggerExit(Collider other) {
         GetComponent<AudioSource>().Stop();
+        uiObject.SetActive(false);
+
     }
 }
